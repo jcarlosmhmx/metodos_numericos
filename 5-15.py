0144f61f69051973f9cad039221b98c9941c4d91
jcarlosmhmx <jcarlosmh@gmail.com>
2023-09-16 12:00:48 -0600

    preliminar

--- 5-15.py
+++ 5-15.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 #----------------------Funciones------------------------
-def f(y):
-    
-    #valor = 1-(g**2)*(b)/(g*(3*y+(y**2/2)**3))
-    return  1-((g**2)/(g*((Ac(y))**3))*b) 
+def funcion(x):
+    return  1-((g**2)/(g*((Ac(x))**3))*(3+x)) 
+    #return x**2 - 4
     
 
-def Ac(y):
-    return 3*y+((y**2)/2)
+def Ac(x):
+    return 3*x+((x**2)/2)
+
+def biseccion(a, b, tolerancia):
+    index=0
+    if funcion(a) * funcion(b) >= 0:
+        raise("Error: La función debe cambiar de signo en el intervalo [a,b]")
+    
+    while(b-a):
+        index=index+1
+        c=(a+b)/2
+        print (index," ",a," ",funcion(a)," ",b," ",funcion(b)," ",c," ",funcion(c))
+
+
+        if funcion(c)==0:
+            return c
+        elif funcion(c)*funcion(a) <0:
+            b=c
+        else:
+            a=c
+    return (a+b)/2
 #----------------------Variables------------------------
-q = 20.00
-g = 9.81
+g=9.81
 
-#Ac = 3*y+((y**2)/2)
+x=1
 
-y=2
-b = 3 + y
+#intervalo inicial
+a=0.5
+#intervalo final
+b=2.5
+#tolerancia
+tolerancia = 0.00001
 
 
-print(b," ",y," ",Ac(y)," ",f(y))
+biseccion(a,b,tolerancia)
