623400c04e3c5172939226476b8c4d13a1aab21d
jcarlosmhmx <jcarlosmh@gmail.com>
2023-09-16 22:56:40 -0600

    cambios

--- 6-9.py
+++ 6-9.py
@@ -7,9 +7,15 @@ def f_prime(x):
     return 2.85 * x**2 - 11.8 * x + 10.9
 
 def newton_raphson(x0, iterations):
-    for _ in range(iterations):
-        
+    i=1
+    
+    #Agregamos el header:
+    print("Index\tx0\tf(x0)")
+
+    for i in range(iterations):
+        print (i, "\t",x0,"\t",f(x0))
         x0 = x0 - f(x0) / f_prime(x0)
+        i = i+1
     return x0
 
 # Suponiendo un valor inicial cercano a la raíz real más grande
@@ -17,4 +23,7 @@ valor_inicial = 3.0
 iteraciones = 3
 
 raiz_aproximada = newton_raphson(valor_inicial, iteraciones)
+print("----------------------------------------------------------------------------")
 print(f"La raíz aproximada es: {raiz_aproximada}")
+print("----------------------------------------------------------------------------")
+
