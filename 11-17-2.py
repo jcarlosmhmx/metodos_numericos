8f8d54dd4fdfd75b318b6104defc48913b9a923e
jcarlosmhmx <jcarlosmh@gmail.com>
2023-09-24 20:33:14 -0600

    nueva version

--- 11-17-2.py
+++ 11-17-2.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 """
-Este módulo utiliza el método numérico Guess Seidel para reolver la ecuación descrita.
+Este módulo utiliza el método numérico Jacobi para reolver la ecuación descrita.
 Author: Equipo 8 Métodos Numéricos
 Date: September 24, 2023
 """
-def gauss_seidel(A, b, x0=None, tol=1e-10, max_iter=1000):
+def jacobi(A, b, x0=None, max_iter=1000, tol=1e-10):
     n = len(b)
     x = x0 or [0.0] * n
 
+    D = [A[i][i] for i in range(n)]
+    R = [[A[i][j] if i != j else 0 for j in range(n)] for i in range(n)]
+
     for k in range(max_iter):
-        x_new = x.copy()
-        for i in range(n):
-            s1 = sum(A[i][j] * x_new[j] for j in range(i))
-            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
-            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+        x_new = [(b[i] - sum(R[i][j] * x[j] for j in range(n))) / D[i] for i in range(n)]
 
         if all(abs(x_new[i] - x[i]) < tol for i in range(n)):
             return x_new
@@ -23,7 +22,7 @@ def gauss_seidel(A, b, x0=None, tol=1e-10, max_iter=1000):
         # Imprime el resultado de la iteración actual
         print(f"Iteración {k + 1}: t = {x[0]}, r = {x[1]}, c = {x[2]}")
 
-    raise ValueError("El método de Gauss-Seidel no convergió")
+    raise ValueError("El método de Jacobi no convergió")
 
 # Coeficientes de las ecuaciones
 A = [[4, 3, 2],
@@ -34,4 +33,7 @@ A = [[4, 3, 2],
 b = [960, 510, 610]
 
 # Resuelve el sistema
-solucion = gauss_seidel(A, b)
+solucion = jacobi(A, b)
+
+# Imprime la solución
+print(f"x = {solucion[0]}, y = {solucion[1]}, z = {solucion[2]}")
