46089d88eab894326acbb41a7c12e7ec46fbdc65
jcarlosmhmx <jcarlosmh@gmail.com>
2023-10-05 09:59:28 -0600

    nueva version

--- 22-15.py
+++ 22-15.py
@@ -11,7 +11,7 @@ def H(x):
     valores = [0, 1.9, 2, 2, 2.4, 2.6, 2.25, 1.12, 0]
     return valores[int(x/2)]
 
-# Implementamos el método de Romberg
+# Implementamos el método de Romberg con impresión de iteraciones
 def romberg(a, b, n):
     R = [[0] * (n+1) for _ in range(n+1)]
     h = (b - a)
@@ -23,13 +23,17 @@ def romberg(a, b, n):
         suma = sum(H(a + k * h) for k in range(1, 2**i, 2))
         R[i][0] = 0.5 * R[i-1][0] + suma * h
 
+        print(f"Iteración {i}:")
+        print(f"  R({i},0): {R[i][0]}")
+
         for j in range(1, i+1):
             R[i][j] = R[i][j-1] + (R[i][j-1] - R[i-1][j-1]) / ((4**j) - 1)
 
+            print(f"  R({i},{j}): {R[i][j]}")
+
     return R[n][n]
 
-# Llamamos a la función Romberg para calcular la integral
+# Llamamos a la función Romberg para calcular la integral e imprimir las iteraciones
 resultado = romberg(0, 16, 4)
 
 print(f"\nEl área de la sección transversal del río es aproximadamente: {resultado} unidades cuadradas.")
-
